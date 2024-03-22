75b52a493ec41ce8e9ed02e161813760bd8f3814
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-22 21:51:34 +0300

    core : IRepositoryUser создан

---
+++ Task-True-Code-Set-02-Db-Requests/Core/Interfaces/IRepositoryUser.cs
@@ -0,0 +1,10 @@
+﻿using Core.Domain;
+using Core.Pagination;
+
+namespace Core.Interfaces;
+public interface IRepositoryUser
+{
+    IEnumerable<User> GetUsersBy(Guid userId, string domain);
+    IEnumerable<User> GetUsersBy(string domain, PaginationSet pagination);
+    IEnumerable<User> GetUsersBy(string tagValue);
+}
