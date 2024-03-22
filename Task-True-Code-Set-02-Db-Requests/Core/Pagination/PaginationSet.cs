fda3d9caf07ee2affc34e66aa323f679f28dd372
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-22 21:51:10 +0300

    core : PaginationSet добавлен

---
+++ Task-True-Code-Set-02-Db-Requests/Core/Pagination/PaginationSet.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Pagination;
+public class PaginationSet
+{
+    public int PageSize { get; set; }
+    public int CurrentPage { get; set; }
+    public string CollationName { get; set; } = default!;
+    public string CollationType { get; set; } = default!;
+}
