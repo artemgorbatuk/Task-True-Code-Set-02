8f047c4b1543fdbb6d146c631bdfeeb346bffa70
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-22 22:20:54 +0300

    datasource : добавлены фабрики управления контекстом базы данных

---
+++ Task-True-Code-Set-02-Db-Requests/Datasource/Factories/DbContextTrueCodeFactoryDesignTime.cs
@@ -0,0 +1,31 @@
+﻿using Datasource.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasource.Factories;
+public class DbContextTrueCodeFactoryDesignTime : IDesignTimeDbContextFactory<DbContextTrueCode>
+{
+    public DbContextTrueCode CreateDbContext(string[] args)
+    {
+        var pathtoApi = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "Api"));
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(pathtoApi)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("Docker");
+
+        var optionsBuilder = new DbContextOptionsBuilder<DbContextTrueCode>();
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new DbContextTrueCode(optionsBuilder.Options);
+    }
+}
---
+++ Task-True-Code-Set-02-Db-Requests/Datasource/Factories/DbContextTrueCodeFactoryScope.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasource.Factories;
+public class DbContextTrueCodeFactoryScope : IServiceScopeFactory
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public DbContextTrueCodeFactoryScope(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public IServiceScope CreateScope()
+    {
+        return _serviceScopeFactory.CreateScope();
+    }
+}
