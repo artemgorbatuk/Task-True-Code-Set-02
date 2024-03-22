a1303865f7dfb3548b2e065228d177b908c5fe17
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-22 22:09:32 +0300

    datasource : DbContextTrueCode создан

---
+++ Task-True-Code-Set-02-Db-Requests/Datasource/Contexts/DbContextTrueCode.cs
@@ -0,0 +1,32 @@
+﻿using Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasource.Contexts;
+public class DbContextTrueCode : DbContext
+{
+    public DbContextTrueCode(DbContextOptions<DbContextTrueCode> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<TagToUser> TagToUser { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.EnableSensitiveDataLogging(false);
+        options.UseLazyLoadingProxies(false);
+        options.UseChangeTrackingProxies(false);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.HasDefaultSchema("dto");
+        base.OnModelCreating(modelBuilder);
+    }
+}
