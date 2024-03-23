d5e04d4e995d1e1f3a7e24d29e9fcef09dc93875
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-23 14:30:45 +0300

    core : добавлено UserEntity

---
+++ Task-True-Code-Set-02-Db-Requests/Core/Models/UserEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models;
+public class UserEntity
+{
+    public Guid UserId { get; set; }
+    public string Name { get; set; } = default!;
+    public string Domain { get; set; } = default!;
+    public IEnumerable<UserTagEntity>? UserTags { get; set; }
+}
+public class UserTagEntity
+{
+    public Guid TagId { get; set; }
+
+    public string Value { get; set; } = default!;
+
+    public string Domain { get; set; } = default!;
+}
+
+public class UserIncludes
+{
+    public bool IsTagsIncluded { get; set; }
+}
