af62a6cbf08cb956ecaf531f4d335c54517053ce
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-23 23:48:08 +0300

    datasource : ConfigurationUser и ConfigurationTag были добавлены

---
+++ Task-True-Code-Set-02-Db-Requests/Datasource/Configurations/ConfigurationTag.cs
@@ -0,0 +1,21 @@
+﻿using Datasource.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasource.Configurations;
+internal class ConfigurationTag : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder
+            .HasMany(ptu => ptu.TagToUsers)
+            .WithOne(t => t.Tag)            
+            .HasForeignKey(ptu => ptu.TagId);
+    }
+}
---
+++ Task-True-Code-Set-02-Db-Requests/Datasource/Configurations/ConfigurationUser.cs
@@ -0,0 +1,20 @@
+﻿using Datasource.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datasource.Configurations;
+internal class ConfigurationUser : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .HasMany(ptu => ptu.TagToUsers)
+            .WithOne(t => t.User)
+            .HasForeignKey(ptu => ptu.UserId);
+    }
+}
