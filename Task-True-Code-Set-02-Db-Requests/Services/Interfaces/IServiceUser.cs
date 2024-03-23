2f03fed9e7bf9ac429ef32762e3e6c36e37cabc7
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-23 23:06:24 +0300

    services : изменен IServiceUser

--- Task-True-Code-Set-02-Db-Requests/Services/Interfaces/IServiceUser.cs
+++ Task-True-Code-Set-02-Db-Requests/Services/Interfaces/IServiceUser.cs
@@ -1,5 +1,7 @@
 ﻿using Core.Models;
+using Core.Pagination;
 using Services.Functionality;
+using Services.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,9 @@ using System.Threading.Tasks;
 namespace Services.Interfaces;
 public interface IServiceUser
 {
-    //RequestResult<IEnumerable<UserEntity>> GetUserBy(Guid userId, string domain);
+    RequestResult<ScreenUserGet> GetUserBy(Guid userId, string domain);
+
+    RequestResult<ScreenUserPaginatedGet> GetUserBy(string domain, PaginationSet paginationSet);
+
+    RequestResult<ScreenUserGet> GetUserBy(string tagValue);
 }
