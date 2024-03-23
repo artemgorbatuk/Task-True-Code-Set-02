5b72a89e739c2455b553b03c25c5172e7da24164
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-23 14:36:58 +0300

    core : добавление extensions для IQueryable

---
+++ Task-True-Code-Set-02-Db-Requests/Core/DataManipulation/WhereIQueryableExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataManipulation;
+
+/// <summary>
+/// Расширения для IQueryable Where 
+/// Методы выполняют следующую функциию 
+/// Когда несколько нескольк фильтров приходит с UI
+/// Все фильтры должны быть применены.
+/// </summary>
+public static class WhereIQueryableExtensions
+{
+    public static IQueryable<TEntityModel> WhereString<TEntityModel>(this IQueryable<TEntityModel> query, string value, Func<string, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return isRequired ? query.Where(x => false) : query;
+        }
+
+        return query.Where(expression(value.Trim()));
+    }
+
+    public static IQueryable<TEntityModel> WhereIntNullable<TEntityModel>(this IQueryable<TEntityModel> query, int? value, Func<int, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IQueryable<TEntityModel> WhereDecimalNullable<TEntityModel>(this IQueryable<TEntityModel> query, decimal? value, Func<decimal, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IQueryable<TEntityModel> WhereYesNoList<TEntityModel>(this IQueryable<TEntityModel> query, List<string> yesNo, Func<List<bool>, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IQueryable<TEntityModel> WhereYesNoString<TEntityModel>(this IQueryable<TEntityModel> query, string yesNo, Func<bool, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IQueryable<TEntityModel> WhereDateRange<TEntityModel>(this IQueryable<TEntityModel> query, DateTime? from, DateTime? to, Func<DateTime, DateTime, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IQueryable<TEntityModel> WhereDateTimeNullable<TEntityModel>(this IQueryable<TEntityModel> query, DateTime? value, Func<DateTime, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IQueryable<TEntityModel> WhereList<TEntityModel, TValue>(this IQueryable<TEntityModel> query, List<TValue> values, Func<List<TValue>, Expression<Func<TEntityModel, bool>>> expression, bool isRequired = false)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Task-True-Code-Set-02-Db-Requests/Core/Pagination/PaginationIQueryableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Pagination;
+
+public static class PaginationIQueryableExtensions
+{
+    public static PaginationResult<TEntityModel> ToPage<TEntityModel>(this IQueryable<TEntityModel> query, PaginationSet paginationSet)
+    {
+        var currentPage = paginationSet.CurrentPage > 0
+            ? paginationSet.CurrentPage
+            : 1;
+
+        var count = query.Count();
+
+        var totalPages = count % paginationSet.PageSize == 0
+            ? count / paginationSet.PageSize
+            : count / paginationSet.PageSize + 1;
+
+        if (currentPage > totalPages)
+        {
+            currentPage = 1;
+        }
+
+        var filteredResult = query
+            .Skip(paginationSet.PageSize * (currentPage - 1))
+            .Take(paginationSet.PageSize);
+
+        return new PaginationResult<TEntityModel>
+        {
+            TotalRecords = count,
+            FilteredResult = filteredResult
+        };
+    }
+}
